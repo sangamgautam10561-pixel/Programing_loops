a8cc202cc011474db8f5b681de372dbc0ae75ee2
Sangam Gautam <sangamgautam10561@gmail.com>
2025-10-29 23:42:35 +0530

    Create Loops &n times.c

---
+++ Loops &n times.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+   printf("Hello sangam\n");
+   
+   printf("n");
+   
+
+    return 0;
+}
